4977cbe180762aa349c5dfdeb81f3618584d9a63
frontinorz <frontinorz@gmail.com>
2020-12-23 17:32:44 +0800

    feat(tutorial): learning Storybook intro

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,9 @@
 import '../src/scss/utils/_normalize.scss'
 import '../src/scss/layout/_layout.scss'
 
+// 教學用的 css
+import '../src/scss/utils/_tutorial.scss'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
