2cf3ef59250ccb2f32641eb7ea9ebd40997a93f2
frontinorz <frontinorz@gmail.com>
2020-12-24 14:54:49 +0800

    Feat(testing): add vue-jest, addon-jest

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
+    '@storybook/addon-jest'
   ],
   // webpack 設定
   webpackFinal: async (config, { configType }) => {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/vue';
+import { withTests } from '@storybook/addon-jest';
+import results from '../.jest-test-results.json';
+import { themes } from '@storybook/theming';
+
 // 引入全域使用的 css
 import '../src/scss/utils/_normalize.scss'
 import '../src/scss/layout/_layout.scss'
@@ -6,5 +11,17 @@ import '../src/scss/layout/_layout.scss'
 import '../src/scss/utils/_tutorial.scss'
 
 export const parameters = {
+  // 設定 Parameter action 的排列方式
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // 設定 Docs 視覺主題
+  docs: {
+    theme: themes.dark,
+  },
 }
+
+// 加入 decorator 對測試的支援
+addDecorator(
+  withTests({
+    results,
+  })
+);
