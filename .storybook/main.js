6104a864f14da976df5616d683f34a87fae6e79b
frontinorz <frontinorz@gmail.com>
2020-12-24 13:18:38 +0800

    feat(addons): Add a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,21 +7,18 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
   ],
+  // webpack 設定
   webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
+    // sass 支援
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../'),
     });
 
-    // Return the altered config
     return config;
   },
 }
